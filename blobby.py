6b68804f3987202b0e6aaa78fac23e87d1a9be2d
Micah Nagel <micah.nagel@defenseunicorns.com>
2023-02-16 09:13:56 -0700

    Add old man yelling at

--- blobby.py
+++ blobby.py
@@ -17,20 +17,26 @@ def validpath(string):
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", help="Image to decorate", type=file, required=True)
 parser.add_argument("--show", help="Show image instead of saving to file", action="store_true")
+parser.add_argument("--yell", help="Make old man yelling at X emoji instead of blob", action="store_true")
 parser.add_argument("-o", "--output", help="Output image (default is ./blob-<image>.png)", type=validpath)
 args = parser.parse_args()
 
 # Constants
 IMGSIZE = 170  # Size of final image (square)
+YELL_IMGSIZE = 128  # Size of final image (square)
 SCALAR = 16 # Amount to oversize temporary images so resampling will not show jagginess.  Should be exponential to 4.
 SCRIPT_DIR = os.path.dirname(__file__)
 BLOB_FILE = f"{SCRIPT_DIR}/icons/blob.png"
+YELL_FILE = f"{SCRIPT_DIR}/icons/old-man-yells-at.png"
 LOGO_SIZE = (78,78)
+YELL_LOGO_SIZE = (50,50)
 
-# Final image
+if args.yell:
+  IMGSIZE = YELL_IMGSIZE
+  BLOB_FILE = YELL_FILE
+  LOGO_SIZE = YELL_LOGO_SIZE
+
 imgfinal = Image.new("RGBA", (IMGSIZE,IMGSIZE))
-
-# Base image
 imgbase = Image.open(BLOB_FILE).convert("RGBA")
 imgbase.thumbnail((IMGSIZE, IMGSIZE))
 imgfinal.paste(imgbase, (int((IMGSIZE - imgbase.width)/2), int((IMGSIZE - imgbase.height)/2)), imgbase) # Centered
