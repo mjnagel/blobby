ee3f36de3d88331cd0d9c11968cde108a99dfd37
EzraNagel <ezra.bagel@gmail.com>
2024-07-19 21:05:48 -0400

    Ensure singular use of yell or cheer and refactor code

--- blobby.py
+++ blobby.py
@@ -14,42 +14,6 @@ def validpath(string):
   else:
     raise argparse.ArgumentTypeError(f"{string} does not point to an existing directory.")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--image", help="Image to decorate", type=file, required=True)
-parser.add_argument("--show", help="Show image instead of saving to file", action="store_true")
-parser.add_argument("--yell", help="Make old man yelling at X emoji instead of blob", action="store_true")
-parser.add_argument("--cheer", help="Show image of blob cheering instead of normal blob", action="store_true")
-parser.add_argument("-o", "--output", help="Output image (default is ./blob-<image>.png or ./old-man-yells-at-<image>.png)", type=validpath)
-args = parser.parse_args()
-
-# Constants
-IMGSIZE = 170  # Size of final image (square)
-YELL_IMGSIZE = 128  # Size of final image (square)
-CHEER_IMGSIZE = 170  # Size of final image (square)
-SCALAR = 16 # Amount to oversize temporary images so resampling will not show jagginess.  Should be exponential to 4.
-SCRIPT_DIR = os.path.dirname(__file__)
-BLOB_FILE = f"{SCRIPT_DIR}/icons/blob.png"
-YELL_FILE = f"{SCRIPT_DIR}/icons/old-man-yells-at.png"
-CHEER_FILE = f"{SCRIPT_DIR}/icons/blob-cheer.gif"
-LOGO_SIZE = (78,78)
-YELL_LOGO_SIZE = (50,50)
-CHEER_LOGO_SIZE = (60,60)
-FILE_BASE = 'blob-'
-YELL_FILE_BASE = 'old-man-yells-at-'
-CHEER_FILE_BASE = 'blob-cheer-'
-
-if args.yell:
-  IMGSIZE = YELL_IMGSIZE
-  BLOB_FILE = YELL_FILE
-  LOGO_SIZE = YELL_LOGO_SIZE
-  FILE_BASE= YELL_FILE_BASE
-
-if args.cheer:
-    IMGSIZE = CHEER_IMGSIZE
-    BLOB_FILE = CHEER_FILE
-    LOGO_SIZE = CHEER_LOGO_SIZE
-    FILE_BASE= CHEER_FILE_BASE
-
 def process_frame(frame, logo, IMGSIZE, LOGO_SIZE): # Process individual gif frames
     frame = frame.convert("RGBA")
     frame.thumbnail((IMGSIZE, IMGSIZE))
@@ -59,16 +23,61 @@ def process_frame(frame, logo, IMGSIZE, LOGO_SIZE): # Process individual gif fra
     imgfinal.paste(logo, (0, 0), logo)  # Top left corner aligned
     return imgfinal
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--image", help="Image to decorate", type=file, required=True)
+parser.add_argument("--show", help="Show image instead of saving to file", action="store_true")
+parser.add_argument("--yell", help="Make old man yelling at X emoji instead of blob", action="store_true")
+parser.add_argument("--cheer", help="Show image of blob cheering instead of normal blob", action="store_true")
+parser.add_argument("-o", "--output", help="Output image (default is ./blob-<image>.png or ./old-man-yells-at-<image>.png)", type=validpath)
+args = parser.parse_args()
 
-logo = Image.open(args.image).convert("RGBA")
-if args.cheer: # Gif processing
+
+# Constants
+IMGSIZE = 170  # Size of final image (square)
+YELL_IMGSIZE = 128  # Size of final image (square)
+CHEER_IMGSIZE = 170  # Size of final image (square)
+SCALAR = 16 # Amount to oversize temporary images so resampling will not show jagginess.  Should be exponential to 4.
+LOGO_SIZE = (78,78)
+YELL_LOGO_SIZE = (50,50)
+CHEER_LOGO_SIZE = (60,60)
+SCRIPT_DIR = os.path.dirname(__file__)
+BLOB_FILE = f"{SCRIPT_DIR}/icons/blob.png"
+YELL_FILE = f"{SCRIPT_DIR}/icons/old-man-yells-at.png"
+CHEER_FILE = f"{SCRIPT_DIR}/icons/blob-cheer.gif"
+FILE_BASE = 'blob-'
+YELL_FILE_BASE = 'old-man-yells-at-'
+CHEER_FILE_BASE = 'blob-cheer-'
+
+# Check if both --yell and --cheer are provided
+if args.yell and args.cheer:
+    print("Error: Only one of --yell or --cheer can be provided, not both.")
+    parser.print_help()
+    exit(1)
+
+if args.yell:
+  IMGSIZE = YELL_IMGSIZE
+  BLOB_FILE = YELL_FILE
+  LOGO_SIZE = YELL_LOGO_SIZE
+  FILE_BASE= YELL_FILE_BASE
+elif args.cheer:
+  IMGSIZE = CHEER_IMGSIZE
+  BLOB_FILE = CHEER_FILE
+  LOGO_SIZE = CHEER_LOGO_SIZE
+  FILE_BASE= CHEER_FILE_BASE
+
+
+if args.cheer:
+    # Process GIF
     img = Image.open(BLOB_FILE)
     frames = []
     durations = []
+    logo = Image.open(args.image).convert("RGBA")
+    logo.thumbnail(LOGO_SIZE)
+
     for frame in ImageSequence.Iterator(img):
         durations.append(frame.info.get('duration', 100))  # Get frame duration
-        frames.append(process_frame(frame, logo, IMGSIZE, LOGO_SIZE)) # Adds image to individual frame and appends
-    
+        frames.append(process_frame(frame, logo, IMGSIZE, LOGO_SIZE))
+
     if args.show:
         frames[0].show()
     else:
@@ -77,17 +86,19 @@ if args.cheer: # Gif processing
         else:
             outfile = './' + FILE_BASE + os.path.splitext(os.path.basename(args.image))[0] + '.gif'
         frames[0].save(outfile, save_all=True, append_images=frames[1:], loop=0, duration=durations, disposal=2)
-        print("Blobified logo saved to " + outfile)
-else: # PNG processing
+        print("Blobified GIF saved to " + outfile)
+else:
+    # Process PNG
     imgfinal = Image.new("RGBA", (IMGSIZE, IMGSIZE))
     imgbase = Image.open(BLOB_FILE).convert("RGBA")
     imgbase.thumbnail((IMGSIZE, IMGSIZE))
-    imgfinal.paste(imgbase, (int((IMGSIZE - imgbase.width) / 2), int((IMGSIZE - imgbase.height) / 2)), imgbase)  # Centered
+    imgfinal.paste(imgbase, (int((IMGSIZE - imgbase.width) / 2), int((IMGSIZE - imgbase.height) / 2)), imgbase)
 
     # Add Logo
+    logo = Image.open(args.image).convert("RGBA")
     logo.thumbnail(LOGO_SIZE)
-    imgfinal.paste(logo, (0, 0), logo)  # Top left corner aligned
-    
+    imgfinal.paste(logo, (0, 0), logo)
+
     if args.show:
         imgfinal.show()
     else:
